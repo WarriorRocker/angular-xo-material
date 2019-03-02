2ce67c647170ab392d5d21cd7d50853bf7ec9e39
Robert Brown <robertbrown@zadv.com>
2019-03-02 15:15:45 -0500

    Add new local environment file and config for running through ng serve and updated npm run serve command

---
+++ src/environments/environment.local.ts
@@ -0,0 +1,18 @@
+export const environment = {
+  production: false
+};
+
+const appConfig = {
+	"app": {
+		"title": "Xo for Angular",
+		"url": "http://angularxo.local",
+		"version": "0.1.2",
+		"debug": true
+	},
+	"paths": {
+		"apiUrl": "http://angularxo.local/xo-api/",
+		"templateUrl": "http://angularxo.local/wp-content/themes/angular-xo-material/"
+	}
+};
+
+window['appConfig'] = appConfig;
