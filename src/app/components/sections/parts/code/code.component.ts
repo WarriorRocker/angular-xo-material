85f5e2b7277fe814850a0715c53e2517c4012531
Robert Brown <robertbrown@zadv.com>
2019-03-05 13:56:28 -0500

    Update codemirror config on code section

--- src/app/components/sections/parts/code/code.component.ts
+++ src/app/components/sections/parts/code/code.component.ts
@@ -1,10 +1,23 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+import { EditorConfiguration } from 'codemirror';
 
 @Component({
 	selector: 'xo-material-sections-code',
 	templateUrl: './code.component.html',
 	styleUrls: ['./code.component.scss']
 })
-export class XoMaterialCodeSectionComponent {
+export class XoMaterialCodeSectionComponent implements OnInit {
   @Input() section: any;
+
+  editorConfiguration: EditorConfiguration = {
+    viewportMargin: Infinity,
+    lineWrapping: true,
+    readOnly: true
+  };
+
+  ngOnInit() {
+    if ((this.section != null) && (this.section.mode != null))
+      this.editorConfiguration.mode = this.section.mode
+  }
 }
